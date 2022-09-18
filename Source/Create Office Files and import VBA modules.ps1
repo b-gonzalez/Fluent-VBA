6e0a963f07c439320f24b32bed68aed1e05a6901
b-gonzalez <numbersarecrazy@gmail.com>
2022-09-18 09:41:57 -0700

    Moved Get-ExcelGuid to top of PowerShell file to fix bug related to running function. Added code to add Scripting.Dictionary library programmatically. Commented out code that adds this until I refactor the Fluent VBA code to use early binding.

--- Source/Create Office Files and import VBA modules.ps1
+++ Source/Create Office Files and import VBA modules.ps1
@@ -8,83 +8,6 @@
 # NOTE: Get-ExcelGuid prompts Excel to save. I can't figure out how to prevent this.
 # Application.DisplayAlerts does not appear to disable it.
 
-try {
-    $curDir = $PSScriptRoot
-    $parentDir = (get-item $curDir).parent
-    Set-Location $parentDir
-    $outputPath = "$parentDir\Distribution\Fluent VBA"
-    $guidObj = Get-ExcelGuid
-    $GuidStr = Out-String -NoNewline -InputObject $guidObj
-    $xlGuid = $GuidStr.Replace("System.__ComObject","")
-
-    $excel = New-Object -ComObject excel.application
-    $word = New-Object -ComObject word.application
-    $powerpoint = New-Object -ComObject powerpoint.application
-    $acc = New-Object -ComObject Access.Application
-
-    $workbook = $excel.Workbooks.Add()
-    $doc = $word.documents.add()
-    $presentation = $powerpoint.Presentations.Add()
-
-    $xlOpenXMLWorkbookMacroEnabled = 52
-    $wdFormatFlatXMLMacroEnabled = 13
-    $ppSaveAsOpenXMLPresentationMacroEnabled = 25
-    $acFileFormatAccess2007 = 12
-
-    $workbook.SaveAs($outputPath, $xlOpenXMLWorkbookMacroEnabled)
-    $doc.SaveAs($outputPath,$wdFormatFlatXMLMacroEnabled)
-    $presentation.SaveAs($outputPath,$ppSaveAsOpenXMLPresentationMacroEnabled)
-    $acc.NewCurrentDataBase($outputPath,$acFileFormatAccess2007)
-    
-    $acModule = 5
-    $macros = Get-ChildItem -Path .\Source -File
-
-    $Major = 0
-    $Minor = 0
-
-    foreach ($macro in $macros) {
-        if ($macro.Extension -ne ".doccls" -and $macro.Extension -ne ".ps1" -and $macro.BaseName -ne "mTODO") {
-            
-            $workbook.VBProject.VBComponents.Import($macro.FullName) | Out-Null
-            $doc.VBProject.VBComponents.Import($macro.FullName)
-            $presentation.VBProject.VBComponents.Import($macro.FullName)
-            $acc.Application.LoadFromText($acModule, $macro.BaseName,$macro)
-        }
-    }
-
-    $doc.VBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
-    $presentation.VBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
-    $acc.VBE.ActiveVBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
-
-}
-
-Catch {
-    Write-Host "An error occurred:"
-    Write-Host $_
-}
-
-Finally {
-    $workbook.Save()
-    $doc.Save()
-    $presentation.Save()
-
-    #$workbook.Close()
-    #$doc.Close()
-    #$presentation.Close()
-    #$acc.CloseCurrentDatabase()
-    
-    $excel.Quit()
-    $word.Quit()
-    $powerpoint.Quit()
-    $acc.Quit()
-
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($word)
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($powerpoint)
-    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($acc)
-    [GC]::Collect()
-}
-
 function Get-ExcelGuid {
     try {
         #$curDir = $PSScriptRoot
@@ -129,4 +52,86 @@ function Get-ExcelGuid {
         [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
         [GC]::Collect()
     }
+}
+
+try {
+    $curDir = $PSScriptRoot
+    $parentDir = (get-item $curDir).parent
+    Set-Location $parentDir
+    $outputPath = "$parentDir\Distribution\Fluent VBA"
+    $guidObj = Get-ExcelGuid
+    $GuidStr = Out-String -NoNewline -InputObject $guidObj
+    $xlGuid = $GuidStr.Replace("System.__ComObject","")
+    $ScriptingGuid = "{420B2830-E718-11CF-893D-00A0C9054228}"
+
+    $excel = New-Object -ComObject excel.application
+    $word = New-Object -ComObject word.application
+    $powerpoint = New-Object -ComObject powerpoint.application
+    $acc = New-Object -ComObject Access.Application
+
+    $workbook = $excel.Workbooks.Add()
+    $doc = $word.documents.add()
+    $presentation = $powerpoint.Presentations.Add()
+
+    $xlOpenXMLWorkbookMacroEnabled = 52
+    $wdFormatFlatXMLMacroEnabled = 13
+    $ppSaveAsOpenXMLPresentationMacroEnabled = 25
+    $acFileFormatAccess2007 = 12
+
+    $workbook.SaveAs($outputPath, $xlOpenXMLWorkbookMacroEnabled)
+    $doc.SaveAs($outputPath,$wdFormatFlatXMLMacroEnabled)
+    $presentation.SaveAs($outputPath,$ppSaveAsOpenXMLPresentationMacroEnabled)
+    $acc.NewCurrentDataBase($outputPath,$acFileFormatAccess2007)
+    
+    $acModule = 5
+    $macros = Get-ChildItem -Path .\Source -File
+
+    $Major = 0
+    $Minor = 0
+
+    foreach ($macro in $macros) {
+        if ($macro.Extension -ne ".doccls" -and $macro.Extension -ne ".ps1" -and $macro.BaseName -ne "mTODO") {
+            
+            $workbook.VBProject.VBComponents.Import($macro.FullName) | Out-Null
+            $doc.VBProject.VBComponents.Import($macro.FullName)
+            $presentation.VBProject.VBComponents.Import($macro.FullName)
+            $acc.Application.LoadFromText($acModule, $macro.BaseName,$macro)
+        }
+    }
+
+    $doc.VBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
+    $presentation.VBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
+    $acc.VBE.ActiveVBProject.References.AddFromGuid($xlGuid,$Major, $Minor)
+
+    <#$workbook.VBProject.References.AddFromGuid($ScriptingGuid,$Major, $Minor)
+    $doc.VBProject.References.AddFromGuid($ScriptingGuid,$Major, $Minor)
+    $presentation.VBProject.References.AddFromGuid($ScriptingGuid,$Major, $Minor)
+    $acc.VBE.ActiveVBProject.References.AddFromGuid($ScriptingGuid,$Major, $Minor)#>
+}
+
+Catch {
+    Write-Host "An error occurred:"
+    Write-Host $_
+}
+
+Finally {
+    $workbook.Save()
+    $doc.Save()
+    $presentation.Save()
+
+    #$workbook.Close()
+    #$doc.Close()
+    #$presentation.Close()
+    #$acc.CloseCurrentDatabase()
+    
+    $excel.Quit()
+    $word.Quit()
+    $powerpoint.Quit()
+    $acc.Quit()
+
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($word)
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($powerpoint)
+    [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($acc)
+    [GC]::Collect()
 }
