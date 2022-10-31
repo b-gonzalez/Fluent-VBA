1d77d2080101f9e87707c2e24c09c662d13ecbbc
b-gonzalez <numbersarecrazy@gmail.com>
2022-10-31 16:47:09 -0700

    Updated created compressed files script to correctly pull tag from GitHub, parse version from tag, and increment for new tag for new release.

--- Scripts/Create compressed files and new release.ps1
+++ Scripts/Create compressed files and new release.ps1
@@ -1,12 +1,18 @@
 git pull
 
+[string]$tag = git describe --tags
+[string]$oldTagNumber= ([regex]"\d.+\-").match($tag).groups[0].Value
+[decimal]$oldTagNumber = $oldTagNumber.Substring(0,$oldTagNumber.Length - 3)
+[decimal]$newTagNumber = $oldTagNumber + .01
+
+# Write-Output "tag is ${tag}"
+# Write-Output "Old tag number is ${OldTagNumber}"
+# Write-Output "Old tag number is ${newTagNumber}"
+
 [string]$curDir = $PSScriptRoot
 [string]$parentDir = (get-item $curDir).parent
 [string]$OldDestination = "$parentDir\Fluent-VBA-${OldTagNumber}.zip"
 [string]$NewDestination = "$parentDir\Fluent-VBA-${NewTagNumber}.zip"
-[string]$tag = git describe --tags
-[decimal]$OldTagNumber = $tag.substring(1,$tag.Length -1)
-[decimal]$newTagNumber = $tagNumber + .01
 
 if (Test-Path $OldDestination) {
   Remove-Item $OldDestination
