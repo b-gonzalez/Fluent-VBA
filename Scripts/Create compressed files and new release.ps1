cf2a32f00f9514778a50b537885d13e50fa79226
b-gonzalez <75147430+b-gonzalez@users.noreply.github.com>
2024-05-22 09:01:09 -0400

    Refactored functions and fixed bugs in Create compressed files and new release.ps1

--- Scripts/Create compressed files and new release.ps1
+++ Scripts/Create compressed files and new release.ps1
@@ -1,55 +1,48 @@
-function get-PreviousTagNumber {
-  [string]$tag = git describe --tags
-  [decimal]$oldTagNumber = ([regex]"[\d|\.]+").match($tag).groups[0].Value
-  Write-Output $oldTagNumber
+function get-TagNumbers {
+  [string]$latestTag = git describe --abbrev=0 --tags
+  return $latestTag
+  # [string]$secondLatestTag = git describe --abbrev=0 --tags --exclude="$(git describe --abbrev=0 --tags)"
+  # Write-Output "Second latest tag number: $secondLatestTag"
 }
 
 function get-AndPublishPackage {
   param (
+    [Parameter(Mandatory = $true)][string]$oldTagNumber,
     [Parameter(Mandatory = $true)][string]$newTagNumber
   )
+
+  $curDir = $PSScriptRoot
+  $parentDir = (get-item $curDir).parent.FullName
+  $COF = "$parentDir\Scripts\Create Office Files.ps1"
+  Import-Module $COF
 
   try {
     git pull
   
-    $curDir = $PSScriptRoot
-    $parentDir = (get-item $curDir).parent.FullName
-    $COF = "$parentDir\Scripts\Create Office Files.ps1"
-    Import-Module $COF
-  
-    # [string]$tag = git describe --tags
-    # [decimal]$oldTagNumber = ([regex]"[\d|\.]+").match($tag).groups[0].Value
-    # [decimal]$increm = .01
-    # [decimal]$newTagNumber = $oldTagNumber + $increm
     [string]$curDir = $PSScriptRoot
     [string]$parentDir = (get-item $curDir).parent
     [string]$OldDestination = "$parentDir\Fluent-VBA-${OldTagNumber}.zip"
     [string]$NewDestination = "$parentDir\Fluent-VBA-${NewTagNumber}.zip"
   
-    if ($newTagNumber - $oldTagNumber -eq $increm) {
-      if (Test-Path $OldDestination) {
-        Remove-Item $OldDestination
-      }
-  
-      if (Test-Path $NewDestination) {
-        Remove-Item $NewDestination
-      }
-  
-      get-officeFiles
-      
-      $compress = @{
-        Path             = "$parentDir\Distribution", "$parentDir\test_files"
-        CompressionLevel = "Fastest"
-        DestinationPath  = $NewDestination
-      }
-      
-      Compress-Archive @compress
-      
-      gh release create "v${newTagNumber}" $NewDestination
+    if (Test-Path $OldDestination) {
+      Remove-Item $OldDestination
     }
-    else {
-      Write-Output "Tag number discrepancy"
+  
+    if (Test-Path $NewDestination) {
+      Remove-Item $NewDestination
     }
+  
+    get-officeFiles
+      
+    $compress = @{
+      Path             = "$parentDir\Distribution", "$parentDir\test_files"
+      CompressionLevel = "Fastest"
+      DestinationPath  = $NewDestination
+    }
+      
+    Compress-Archive @compress
+      
+    gh release create "v${newTagNumber}" $NewDestination
   }
   catch {
     Write-Host "An error occurred:"
@@ -57,4 +50,6 @@ function get-AndPublishPackage {
   }
 }
 
-# get-PreviousTagNumbers
+$lastTagNum = get-TagNumbers
+Write-Output "Latest tag number: $lastTagNum"
+get-AndPublishPackage -oldTagNumber $lastTagNum -newTagNumber "2.0.0"
