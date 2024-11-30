48ac2494c599d0c2586586398e4502fa8322d738
Brian Gonzalez <numbersarecrazy@gmail.com>
2024-11-30 07:28:04 -0500

    Added notes, fixed typo bug in build variable, added a commented out line of code for 32-bit dll registration

--- Scripts/register_tb_dll.ps1
+++ Scripts/register_tb_dll.ps1
@@ -1,3 +1,14 @@
+#NOTE: the program that's running this script must be
+#run as administrator e.g. in command prompt,PowerShell, 
+#VSCode, etc. You must also select the correct bitness
+#for the DLL you want to run. The script defaults to
+#64 bit. But if you want to run for 32bit, just comment
+#out the 64bit line and uncomment the 32bit line at the
+#bottom of the file.
+
+#If run successfully, you will get a msgbox
+#notification that dllRegisterServer succeeded.
+
 enum bitness {
   bit32 = 1
   bit64 = 2
@@ -11,7 +22,7 @@ function registerDll(){
   $dllFilename = ""
   $curDir = $PSScriptRoot
   $parentDir = (get-item $curDir).parent.FullName
-  $build = "$parentDir\Source\twin_basic\Bulid"
+  $build = "$parentDir\Source\twin_basic\Build"
   Set-Location $build
 
   if ($dllBitness -eq [bitness]::bit32) {
@@ -23,4 +34,5 @@ function registerDll(){
   C:\Windows\System32\regsvr32.exe $dllFilename
 }
 
+# registerDll -dllBitness bit32
 registerDll -dllBitness bit64
