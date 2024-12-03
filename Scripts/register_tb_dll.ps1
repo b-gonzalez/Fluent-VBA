0c2853209a3658cb75e7247a5d27898a7d1cb947
b-gonzalez <75147430+b-gonzalez@users.noreply.github.com>
2024-12-03 18:27:01 -0500

    Updated notes. Updated bitness enum with the Flags attribute. Deleted code related to bitnessArr. Updated parameter to be dllBitness. Refactored if statements to use dllBitness variable. Updated function calls.

--- Scripts/register_tb_dll.ps1
+++ Scripts/register_tb_dll.ps1
@@ -6,44 +6,36 @@
 #want to run. The script defaults to 64-bit. This is due
 #to the function call using a bit64 parameter at the 
 #bottom of this file. If you want to run for 32-bit, 
-#just comment out the bit64 line and uncomment the bit32 
-#line. If you want to run for both 32-bit and 64-bit, then
-#comment out the 64-bit line and uncomment the line with 
-#bit32, bit64
+#just comment out the ([bitness]::bit64) line and uncomment 
+#the ([bitness]::bit32) line. If you want to run for both 
+#32-bit and 64-bit, then comment out the 64-bit line and 
+#uncomment the line with ([bitness]::bit32 + [bitness]::bit64)
 
 #If run successfully, you will get a msgbox notification
 #that dllRegisterServer succeeded. After that, go to tools
 #> References in the VBIDE and select the DLL file. After
 #that you should be able to use code within the library.
 
-enum bitness {
+[Flags()] enum bitness {
   bit32 = 1
   bit64 = 2
 }
 
 function registerDll(){
   param(
-    [bitness[]]$bitnessArr
+    [bitness]$dllBitness
   )
-
-  [int]$dllInt = 0
-
-  foreach($bitness in $bitnessArr) {
-    $dllInt += [int]([int][bitness]::$bitness)
-  }
   
   $curDir = $PSScriptRoot
   $parentDir = (get-item $curDir).parent.FullName
   $build = "$parentDir\Source\twin_basic\Build"
   Set-Location $build
 
-  [string[]]$arr = @()
-
-  if ($dllInt -band [bitness]::bit32) {
+  if ($dllBitness -band [bitness]::bit32) {
     $arr += "$($build)\fluent_vba_tb_win32.dll"
   } 
   
-  if ($dllInt -band [bitness]::bit64) {
+  if ($dllBitness -band [bitness]::bit64) {
     $arr += "$($build)\fluent_vba_tb_win64.dll"
   }
 
@@ -58,6 +50,6 @@ function registerDll(){
   Set-Location $curDir
 }
 
-# registerDll -bitnessArr bit32
-registerDll -bitnessArr bit64
-# registerDll -bitnessArr bit32, bit64
+# registerDll -dllBitness ([bitness]::bit32)
+registerDll -dllBitness ([bitness]::bit64)
+# registerDll -dllBitness ([bitness]::bit32 + [bitness]::bit64)
